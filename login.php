c6273c4826b5c31322f60f343ad38a12613a3293
Noah French <noahjfrench@gmail.com>
2019-04-15 22:50:14 -0400

    Add basic data collection based on the user who is logged in.

--- login.php
+++ login.php
@@ -2,14 +2,15 @@
 session_start();
 
 function reject($errorType) {
-    $url = 'login_fail.php?error=' . $errorType;
+    $url = './login_fail.php?error=' . $errorType;
     header('Location: ' . $url);
     exit();
 }
 
-function accept() {
+function accept($user) {
     $_SESSION['user'] = $user;
-    header('Location: home.html');
+    header('Location: ./home.html');
+    exit();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -20,13 +21,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($pw != 'password') {
             reject('password');
         } else {
-            accept();
+            accept($user);
         }
     } else if ($user == 'noah') {
         if ($pw != 'password') {
             reject('password');
         } else {
-            accept();
+            accept($user);
         }
     } else if ($user == '') {
         reject('noName');
